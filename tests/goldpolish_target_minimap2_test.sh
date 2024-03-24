3471425b2a4b3f9d23b3ca809c6be58e4079e47c
emily005 <emilyzhang54368@gmail.com>
2024-03-23 18:54:25 -0700

    add gp-t minimap2 ci

---
+++ tests/goldpolish_target_minimap2_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eux -o pipefail
+
+# Download the test data
+curl -L --output test_reads.fq https://www.bcgsc.ca/downloads/btl/goldrush/test/test_reads.fq
+
+
+# Run this demo to test your GoldRush installation
+echo "Launching GoldPolish-Target"
+
+goldpolish --target --target_dev --minimap2 goldpolish_target_test_golden_path.fa test_reads.fq goldpolish_target_test_golden_path.targeted
+
+if cmp --silent -- goldpolish_target_test_golden_path.targeted.polished.fa $(pwd)/expected_files/minimap2/goldpolish_target_test_golden_path.targeted.polished.expected.fa && \
+   cmp --silent -- goldpolish_target_test_golden_path.targeted.gaps.i.fa $(pwd)/expected_files/minimap2/goldpolish_target_test_golden_path.targeted.gaps.expected.fa && \
+   cmp --silent -- goldpolish_target_test_golden_path.targeted.gaps.goldpolished.i.fa $(pwd)/expected_files/minimap2/goldpolish_target_test_golden_path.targeted.gaps.goldpolished.expected.fa; then 
+    echo "Test successful"
+else
+  echo "Final polishing file doesn't match expected result - please check your installation"
+  exit 1
+fi
+exit 0
